ac046f49a3684098e9b765ca2c510bc014d76a63
sharifjon <sharifjon.shukurjonov.2008.22.07@gmail.com>
2025-04-09 14:32:07 +0500

    Homework yakunlandi

--- app.py
+++ app.py
@@ -1,49 +1,6 @@
-# from http.cookiejar import request_path
-
-# from webob import Request, Response
-#
-# class FrameWorkApp:
-#     def __init__(self):
-#         self.routes = dict()
-#     def __call__(self, environ, start_response):
-#         req = Request(environ)
-#         res = self.handle_request(req)
-#         return res(environ, start_response)
-#
-#     def handle_request(self, request):
-#         res = Response()
-#         is_there = False
-#
-#         for path, handler in self.routes.items():
-#             lst = request.path.split("/")
-#             if len(lst) > 2 and lst[1] == "u":
-#                 handler(request,res, lst[2])
-#                 is_there= True
-#             if path == request.path:
-#                 handler(request, res)
-#                 is_there = True
-#
-#         if not is_there:
-#             self.default_response(res)
-#
-#         return res
-#
-#     def default_response(self, response):
-#         response.status_code = 404
-#         response.text = ("ERROR: 404 ... ... ...\n"
-#                          "\nURLda xatolik bor, iltimos tekshiribbir ozdan so'ng urunib ko'ring!")
-#
-#     def route(self, path):
-#         def wrapper(handler):
-#             self.routes["/" + path] = handler
-#             return handler
-#         return wrapper
-
-
-
-"""HOMEWORK"""
-
 from webob import Request, Response
+
+
 
 class FrameWorkApp:
     def __init__(self):
@@ -56,33 +13,40 @@ class FrameWorkApp:
 
     def handle_request(self, request):
         res = Response()
-        is_there = False
+        path = request.path
+        matched = False
 
-        for path, handler in self.routes.items():
-            lst = request.path.split("/")
-            if len(lst) > 2 and lst[1] == "u":
-                handler(request, res, lst[2], is_admin=False)
-                is_there = True
-            elif len(lst) > 2 and lst[1] == "admin":
-                handler(request, res, lst[2], is_admin=True)
-                is_there = True
-            elif path == request.path:
+        for route_path, handler in self.routes.items():
+            lst = path.strip("/").split("/")
+
+            if route_path == "/admin/login" and len(lst) >= 3 and "/".join(lst[:2]) == "admin/login":
+                handler(request, res, lst[2])
+                matched = True
+                break
+
+
+            elif route_path == "/u/id" and len(lst) >= 3 and "/".join(lst[:2]) == "u/id":
+                handler(request, res, lst[2])
+                matched = True
+                break
+
+            elif route_path == path:
                 handler(request, res)
-                is_there = True
+                matched = True
+                break
 
-        if not is_there:
+        if not matched:
             self.default_response(res)
 
         return res
 
-    def default_response(self, response):
+    def default_response(self,response):
         response.status_code = 404
-        response.text = ("ERROR: 404 ... ... ...\n"
-                         "\nURLda xatolik bor, iltimos tekshirib bir ozdan so'ng urunib ko'ring!")
+        response.text = "URL xatolik bor , topilmadi!"
 
     def route(self, path):
         def wrapper(handler):
-            self.routes["/" + path] = handler
+            self.routes[path] = handler
             return handler
-        return wrapper
 
+        return wrapper
--- main.py
+++ main.py
@@ -1,113 +1,52 @@
-
-# from wsgiref.simple_server import make_server
-#
-# def app(environ, start_response):
-#     file_path = "C:\\Users\\shari\\OneDrive\\Desktop\\2025-03-28 15-28-45.mp4"
-#     with open(file_path, "rb") as file:
-#         data = file.read()
-#
-#     status = "200 OK"
-#     response_headers = [("Content-type", "video/mp4")]      # video seerverga chiqarish
-#
-#     start_response(status, response_headers)
-#
-#     return [data]
-#
-# server = make_server("localhost", 8000, app)
-# server.serve_forever()
-
-
-
-
-
-
-# from wsgiref.simple_server import make_server
-# from app import FrameWorkApp
-#
-# app = FrameWorkApp()
-#
-#
-# server = make_server("localhost", 8000, app)       # run bilan ishlash
-# server.serve_forever()
-
-
-
-
-# from app import FrameWorkApp
-# import json
-#
-# def load_date():
-#     with open("users.json", "r") as file:
-#         date = json.load(file)
-#
-# app = FrameWorkApp()
-#
-# @app.route("/home")
-# def home(request, response):
-#     response.text = "Home pagedan siz uchun maxsus salom!"
-#
-# @app.route("/about")
-# def about(request, response):
-#     response.text = "About pagedan siz uchun maxsus salom!"
-#
-# @app.route("/u/{id}")
-# def get_info(request, response, id):
-#     user = load_date()[int(id)]
-#     response.text = json.dumps(user)
-
-
-
-
-
-"""HOMEWORK"""
 from app import FrameWorkApp
+
+
 import json
-
-
-def load_data():
-    with open("users.json", "r") as file:
-        data = json.load(file)
-    return data
-
 
 app = FrameWorkApp()
 
+def load_users():
+    with open( "user.json","r")as file:
+        users = json.load(file)
+    return users
+def load_admin():
+    with open("admin.json", "r") as file:
+        admins = json.load(file)
+    return admins
 
 @app.route("/home")
-def home(request, response):
-    response.text = "Home pagedan siz uchun maxsus salom!"
+def home(request,response):
+    response.text = "shuhrat pagedan alangali salom!"
 
 
 @app.route("/about")
-def about(request, response):
-    response.text = "About pagedan siz uchun maxsus salom!"
+def about (request,response):
+    response.text = "about pageidan alangali salom!"
+
+@app.route("/contact")
+def contact (request,response):
+    response.text = "91 336 2084"
 
 
-@app.route("/u/{id}")
-def get_user_info(request, response, id, is_admin=False):
-    users = load_data()
 
-    try:
-        user = users[int(id)]
-        if not is_admin:
-            response.text = json.dumps(user)
-        else:
-            response.text = "Kirish taqiqlangan: Administrator maʼlumotlarini bu yerda koʻrib boʻlmaydi."
-    except (IndexError, ValueError):
+@app.route("/admin/login")
+def get_admin_info(request, response, login):
+    admins = load_admin()
+    admin = admins.get(login)
+
+    if admin:
+        response.text = json.dumps(admin)
+    else:
+        response.status_code = 404
+        response.text = "Admin topilmadi!"
+
+@app.route("/u/id")
+def get_user_info(request, response, id):
+    users = load_users()
+    user = users.get(id)
+
+    if user:
+        response.text = json.dumps(user)
+    else:
         response.status_code = 404
         response.text = "User topilmadi!"
-
-
-@app.route("/admin/{id}")
-def get_admin_info(request, response, id, is_admin=False):
-    users = load_data()
-
-    try:
-        if is_admin:
-            admin_data = users[int(id)]
-            response.text = json.dumps(admin_data)
-        else:
-            response.text = "Kirish rad etildi: Siz administrator maʼlumotlarini koʻrish huquqiga ega emassiz."
-    except (IndexError, ValueError):
-        response.status_code = 404
-        response.text = "ADMIN topilmadi!"
